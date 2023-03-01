ce28bb3b5b60d40139f4105322b2d3328ca83bad
sAyuri-fam <30927620+sAyuri-fam@users.noreply.github.com>
2023-03-02 00:48:54 +0800

    fix

--- MERN base/server/controllers/userController.js
+++ MERN base/server/controllers/userController.js
@@ -23,10 +23,10 @@ const loginUser = async (req, res) => {
 
 // signup
 const signupUser = async (req, res) => {
-  const {username, password} = req.body;
+  const {username, email, password} = req.body;
 
   try {
-    const user = await User.signup(username, password);
+    const user = await User.signup(username, email, password);
 
     // create a token
     const token = createToken(user._id);
