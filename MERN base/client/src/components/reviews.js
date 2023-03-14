2955eeca893c2a13355e72bbf66adfa0938361c0
bhvrito <113892797+bhvrito@users.noreply.github.com>
2023-03-15 06:38:23 +0800

    Seller Profile Page done

--- MERN base/client/src/components/reviews.js
+++ MERN base/client/src/components/reviews.js
@@ -1,13 +1,208 @@
-import { Container, Heading } from "@chakra-ui/react";
+import { StarIcon } from "@chakra-ui/icons";
+import { Avatar, Container, Heading, HStack, Text, VStack} from "@chakra-ui/react";
 import React from "react";
 
 function Reviews(){
     return(
         <>
-        <Container w='full' h='full' bg='green'>
-            <Heading> Reviews</Heading>
-            <br /><br /><br />
-        </Container>
+        <VStack alignItems="flex-start" spacing={8}>
+            <HStack spacing={5}>
+                <Avatar size="md"></Avatar>
+                <VStack alignItems="flex-start">
+                    <Heading size="sm" color="white">USer12339</Heading>
+                    <HStack>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <Text color="grey" size="0.5em">1 year ago</Text>
+                    </HStack>
+                    <Text color="white">Seller very responsible and kind!</Text>
+                </VStack>
+            </HStack>
+            <HStack spacing={5}>
+                <Avatar size="md"></Avatar>
+                <VStack alignItems="flex-start">
+                    <Heading size="sm" color="white">USer12339</Heading>
+                    <HStack>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <Text color="grey" size="0.5em">1 year ago</Text>
+                    </HStack>
+                    <Text color="white">Seller very responsible and kind!</Text>
+                </VStack>
+            </HStack>
+            <HStack spacing={5}>
+                <Avatar size="md"></Avatar>
+                <VStack alignItems="flex-start">
+                    <Heading size="sm" color="white">USer12339</Heading>
+                    <HStack>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <Text color="grey" size="0.5em">1 year ago</Text>
+                    </HStack>
+                    <Text color="white">Seller very responsible and kind!</Text>
+                </VStack>
+            </HStack>
+            <HStack spacing={5}>
+                <Avatar size="md"></Avatar>
+                <VStack alignItems="flex-start">
+                    <Heading size="sm" color="white">USer12339</Heading>
+                    <HStack>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <Text color="grey" size="0.5em">1 year ago</Text>
+                    </HStack>
+                    <Text color="white">Seller very responsible and kind!</Text>
+                </VStack>
+            </HStack>
+            <HStack spacing={5}>
+                <Avatar size="md"></Avatar>
+                <VStack alignItems="flex-start">
+                    <Heading size="sm" color="white">USer12339</Heading>
+                    <HStack>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <Text color="grey" size="0.5em">1 year ago</Text>
+                    </HStack>
+                    <Text color="white">Seller very responsible and kind!</Text>
+                </VStack>
+            </HStack>
+            <HStack spacing={5}>
+                <Avatar size="md"></Avatar>
+                <VStack alignItems="flex-start">
+                    <Heading size="sm" color="white">USer12339</Heading>
+                    <HStack>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <Text color="grey" size="0.5em">1 year ago</Text>
+                    </HStack>
+                    <Text color="white">Seller very responsible and kind!</Text>
+                </VStack>
+            </HStack>
+            <HStack spacing={5}>
+                <Avatar size="md"></Avatar>
+                <VStack alignItems="flex-start">
+                    <Heading size="sm" color="white">USer12339</Heading>
+                    <HStack>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <Text color="grey" size="0.5em">1 year ago</Text>
+                    </HStack>
+                    <Text color="white">Seller very responsible and kind!</Text>
+                </VStack>
+            </HStack>
+            <HStack spacing={5}>
+                <Avatar size="md"></Avatar>
+                <VStack alignItems="flex-start">
+                    <Heading size="sm" color="white">USer12339</Heading>
+                    <HStack>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <Text color="grey" size="0.5em">1 year ago</Text>
+                    </HStack>
+                    <Text color="white">Seller very responsible and kind!</Text>
+                </VStack>
+            </HStack>
+            <HStack spacing={5}>
+                <Avatar size="md"></Avatar>
+                <VStack alignItems="flex-start">
+                    <Heading size="sm" color="white">USer12339</Heading>
+                    <HStack>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <Text color="grey" size="0.5em">1 year ago</Text>
+                    </HStack>
+                    <Text color="white">Seller very responsible and kind!</Text>
+                </VStack>
+            </HStack>
+            <HStack spacing={5}>
+                <Avatar size="md"></Avatar>
+                <VStack alignItems="flex-start">
+                    <Heading size="sm" color="white">USer12339</Heading>
+                    <HStack>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <Text color="grey" size="0.5em">1 year ago</Text>
+                    </HStack>
+                    <Text color="white">Seller very responsible and kind!</Text>
+                </VStack>
+            </HStack>
+            <HStack spacing={5}>
+                <Avatar size="md"></Avatar>
+                <VStack alignItems="flex-start">
+                    <Heading size="sm" color="white">USer12339</Heading>
+                    <HStack>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <Text color="grey" size="0.5em">1 year ago</Text>
+                    </HStack>
+                    <Text color="white">Seller very responsible and kind!</Text>
+                </VStack>
+            </HStack>
+            <HStack spacing={5}>
+                <Avatar size="md"></Avatar>
+                <VStack alignItems="flex-start">
+                    <Heading size="sm" color="white">USer12339</Heading>
+                    <HStack>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <Text color="grey" size="0.5em">1 year ago</Text>
+                    </HStack>
+                    <Text color="white">Seller very responsible and kind!</Text>
+                </VStack>
+            </HStack>
+            <HStack spacing={5}>
+                <Avatar size="md"></Avatar>
+                <VStack alignItems="flex-start">
+                    <Heading size="sm" color="white">USer12339</Heading>
+                    <HStack>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <StarIcon color="gold"></StarIcon>
+                        <Text color="grey" size="0.5em">1 year ago</Text>
+                    </HStack>
+                    <Text color="white">Seller very responsible and kind!</Text>
+                </VStack>
+            </HStack>
+
+        </VStack>
         </>
     )
 }
