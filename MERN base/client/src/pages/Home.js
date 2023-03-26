47413baa8da3708ca582d3d3a542580c2b05b49e
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-03-26 13:42:38 +0800

    [Frontend] carousel for phishing alerts
    Loop through alerts every few seconds

--- MERN base/client/src/pages/Home.js
+++ MERN base/client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Container, HStack, Heading, Divider, Grid} from "@chakra-ui/react";
 import PhishingAlert from '../components/phishingalert';
 import ListingCard from '../components/ListingCard';
@@ -7,8 +7,33 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Categories from "../components/categories";
 function Home(){
-  //fetch this as a json response. @ Jeremy
+  const [pAIndex, setPAIndex] = useState(1);
+  useEffect(()=>{
+    const intervalId = setInterval(()=>{
+      setPAIndex((pAIndex + 1)%4);
+    },10000);
+  })
   
+  const alerts = [
+    {
+      title:"Protect yourself from phishing scams!",
+      content:"Do not enter your contact, banking details or OTP to receive payment."
+    },
+    {
+      title:"Always check that the items are up to your standards!",
+      content:"You deserve the best and the best only."
+    },
+    {
+      title:"Flagged anonymous or suspicious users to admin!",
+      content:"We all have a responsibility to keep NTU Marketplace safe."
+    },
+    {
+      title:"Be careful not to exchange personal or private details with sellers!",
+      content:"Keep a level of anonymity in order to prevent yourself from being stalked."
+    },
+  ];
+
+  const[item,setItem] = useState(alerts[0]);
   const items = [
     {
       title: "Living Room Sofa",
@@ -54,7 +79,7 @@ function Home(){
     return(
       <>
       <Navbar/>
-      <Header />
+      {/* <Header /> */}
         <Container
         mt='0'
         mb='0'
@@ -62,23 +87,24 @@ function Home(){
         mr='0'
         minWidth='100%'
           >
-        <PhishingAlert/>
+        <PhishingAlert title={alerts[pAIndex].title} content={alerts[pAIndex].content} /> 
+
         <Heading mb={4} size='lg' >Categories</Heading>
         <Divider w='40%'></Divider>
         <Grid 
-                autoFlow='column'
-                autoColumns={['21%', '21%','15%']}
-                w='90vw'
-                overflowX='auto'
-                overflowY='hidden'
-                gap={3}
-                mt='1.5em'
-                mb='1.5em'
-                sx={{
-                    '&::-webkit-scrollbar': {
-                        display : 'hidden'
-                    }
-                }} >
+              autoFlow='column'
+              autoColumns={['21%', '21%','15%']}
+              w='90vw'
+              overflowX='auto'
+              overflowY='hidden'
+              gap={3}
+              mt='1.5em'
+              mb='1.5em'
+              sx={{
+                  '&::-webkit-scrollbar': {
+                      display : 'hidden'
+                  }
+              }} >
         {categoriesItems.map((categoriesItem)=>(
           <Categories
           key={categoriesItem.title}
