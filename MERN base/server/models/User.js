fec2ec42022029fd8e080f984aedf6649b243be2
sAyuri-fam <30927620+sAyuri-fam@users.noreply.github.com>
2023-03-28 21:25:55 +0800

    otp

--- MERN base/server/models/User.js
+++ MERN base/server/models/User.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const validator = require('validator');
 
+
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -23,39 +25,39 @@ const userSchema = new mongoose.Schema({
   mobile: {
     type: String,
     // required: true
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+    required: true
   }
 });
 
 
 // signup
-userSchema.statics.signup = async function(username, email, password) {
+// userSchema.statics.signup = async function(username, email, password) {
 
-  if (!username || !password || !email) {
-    throw Error("All fields must be filled!");
-  }
-  // validate password stregnth
-  // if (!validator.isStrongPassword(password)) { 
-  //   throw Error("Password not strong enough!")
-  // }
+//   if (!username || !password || !email ) {
+//     throw Error("All fields must be filled!");
+//   }
+  
+//   if (!validator.isEmail(email) || !email.endsWith('@e.ntu.edu.sg')) {
+//     throw Error("Please enter a valid NTU email")
+//   }
 
-  if (!validator.isEmail(email) || !email.endsWith('@e.ntu.edu.sg')) {
-    throw Error("Please enter a valid NTU email")
-  }
+//   const isExist = await this.findOne({ username });
+
+//   if (isExist) {
+//     throw Error("Username already in use");
+//   } 
+
+//   const salt = await bcrypt.genSalt(10);
+//   const hash = await bcrypt.hash(password, salt);
+//   const user = await this.create({ username, email, password: hash, verified: false });
 
 
-  const isExist = await this.findOne({ username });
-
-  if (isExist) {
-    throw Error("Username already in use");
-  }
-
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(password, salt);
-
-  const user = await this.create({ username, email, password: hash });
-
-  return user;
-};
+//   return user;
+// };
 
 
 // login
