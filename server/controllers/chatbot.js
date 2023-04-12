8d758923fd0ab99fe49f4d06b0d559dfc3a79941
Pray Apostel <praysimanjuntak@Prays-MacBook-Air.local>
2023-04-12 15:57:28 +0800

    update

--- server/controllers/chatbot.js
+++ server/controllers/chatbot.js
@@ -1,35 +1,19 @@
 const axios = require('axios');
 
-async function fetchGPT3Response(prompt) {
-    const openaiApiKey = process.env.OPEN_AI_API;
-  
-    const response = await axios.post(
-      'https://api.openai.com/v1/engines/davinci-codex/completions',
-      {
-        prompt: prompt,
-        temperature: 0.7,
-        max_tokens: 150,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${openaiApiKey}`,
-        },
-      }
-    );
-  
-    return response.data.choices[0].text.trim();
+async function fetchGPT3Response(openai, prompt) {
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt,
+    });
+    return completion.data.choices[0].text;
 }
 
-const handleBotPrompt = async (req, res) => {
+const handleBotPrompt = (openai) => async (req, res) => {
     const { prompt } = req.body;
     if (!prompt) return res.status(400).json("Please insert a prompt.")
 
     try {
-        const response = await fetchGPT3Response(prompt);
+        const response = await fetchGPT3Response(openai, prompt);
         return res.json(response);
     } catch (e) {
         console.log(e);
